0b3abeba35322f03dd74b66a41be664d74ec7750
jermon <jerker.montelius@gmail.com>
2021-04-01 10:21:31 +0200

    Restructure

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
-package combinatorics
+package main
 
 import(
   "fmt"
+  c "action-test/combinatorics"
 )
 
 func main() {
   fmt.Println("Hello World!")
+  var a1 = []int{0, 1, 2, 3}
+	p1 := c.Perm2(a1)
+	fmt.Printf("Permutation1: \n%v ", p1)
+	fmt.Printf("Nr. of permutations: %v\n", len(p1))
 }
